7bc232bf0ddf3dd70c207a9294a411718067df96
vinivent <vini.ventura999@gmail.com>
2024-09-23 11:11:19 -0300

    removing unused import

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path, include
 from django.http import HttpResponse
 
-from users.views import teste
-
 # Uma função simples para a página inicial
 def home(request):
     return HttpResponse("Bem-vindo à API!")
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('', teste), 
 ]
